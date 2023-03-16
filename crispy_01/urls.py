9f0eaa881aeccbe854a57a4eeb2bc36f8907529a
jmrkfnz <119049235+jmarkfen@users.noreply.github.com>
2023-03-16 22:14:04 +0800

    Fix bootstrap4 dependency

--- crispy_01/urls.py
+++ crispy_01/urls.py
@@ -19,5 +19,5 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", views.TestView.as_view(), name="test"),
+    path("", views.TestForm.as_view(), name="test"),
 ]
--- crispy_01/views.py
+++ crispy_01/views.py
@@ -1,4 +1,10 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, FormView
+from . import forms
 
 class TestView(TemplateView):
     template_name = "form.html"
+
+class TestForm(FormView):
+    template_name = 'form.html'
+    form_class = forms.AddressForm
+    #success_url = '/'
